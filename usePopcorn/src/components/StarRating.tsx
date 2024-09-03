638fc67701a025f49e6e4499b1bc6d8f39264468
ChandraShekhar <csyadav0513@gmail.com>
2024-09-04 00:43:50 +0530

    refactor: Update package dependencies and add lucide-react for better movie rating display

--- usePopcorn/src/components/StarRating.tsx
+++ usePopcorn/src/components/StarRating.tsx
@@ -4,13 +4,13 @@ import { useState } from "react";
 import Star from "./Star";
 
 const StarRating = ({ maxRating = 10 }: any) => {
-  const [rating, setRating] = useState(0);
+  const [rating, setRating] = useState(1);
   return (
     <div className="flex justify-center items-center w-full py-2">
       <div className="flex ">
         {Array.from({ length: maxRating }, (_, i) => (
           <span key={i} className="text-sm ">
-            <Star onClick={() => setRating(i + 1)} />
+            <Star onClick={() => setRating(i + 1)} full={rating >= i + 1} />
           </span>
         ))}
       </div>
