c6b4e98972e6ec0c63f5059386a5e7d694b78503
ChandraShekhar <csyadav0513@gmail.com>
2024-09-03 20:05:55 +0530

    refactor: Improve movie list display and search functionality

--- usePopcorn/src/components/Navbar.tsx
+++ usePopcorn/src/components/Navbar.tsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import Search from "./Search";
+
 
 const Navbar = () => {
-  const [query, setQuery] = useState("");
+  
   return (
     <>
       <nav className="nav-bar">
@@ -9,13 +10,8 @@ const Navbar = () => {
           <span role="img">🍿</span>
           <h1>usePopcorn</h1>
         </div>
-        <input
-          className="search"
-          type="text"
-          placeholder="Search movies..."
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
+        <Search />
+      
         <p className="num-results">
           {/* Found <strong>{movies.length}</strong> results */}
           x
