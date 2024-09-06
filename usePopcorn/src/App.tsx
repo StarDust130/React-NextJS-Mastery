adb139a48ce036f2924a69175dc123e22cfad613
ChandraShekhar <csyadav0513@gmail.com>
2024-09-06 20:04:47 +0530

    refactor: Handle error while fetching movie data from OMDB API in App component

--- usePopcorn/src/App.tsx
+++ usePopcorn/src/App.tsx
@@ -1,27 +1,46 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 import { useEffect, useState } from "react";
 import ListBox from "./components/ListBox";
 import MovieList from "./components/MovieList";
 import Loader from "./components/Loader";
-
+import ErrorMsg from "./components/ErrorMsg";
 
 const KEY = "30c03942";
 
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    async function fetchData() {
-      setIsLoading(true);
-      const response = await fetch(
-        `http://www.omdbapi.com/?i=tt3896198&apikey=${KEY}&s=matrix`
-      );
-      const data = await response.json();
-      setMovies(data.Search);
-      setIsLoading(false);
-    }
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        const response = await fetch(
+          `http://www.omdbapi.com/?i=tt3896198&apikey=${KEY}&s=matrix`
+        );
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+
+        const data = await response.json();
+
+        if (data.Response === "True") {
+          setMovies(data.Search);
+        } else {
+          throw new Error(data.Error);
+        }
+      } catch (error: any) {
+        console.error("Error fetching data: ", error);
+        setError(error?.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
     fetchData();
   }, []);
 
@@ -34,7 +53,13 @@ export default function App() {
       </Navbar>
       <Main>
         <ListBox>
-          {!isLoading ? <Loader /> : <MovieList movies={movies} />}
+          {isLoading ? (
+            <Loader />
+          ) : error ? (
+            <ErrorMsg message={error} />
+          ) : (
+            <MovieList movies={movies} />
+          )}
         </ListBox>
       </Main>
     </>
---
+++ usePopcorn/src/components/ErrorMsg.tsx
@@ -0,0 +1,5 @@
+
+const ErrorMsg = ({message}) => {
+  return <p className="text-red-500 font-bold flex justify-center items-center ">{message}😱</p>;
+}
+export default ErrorMsg
