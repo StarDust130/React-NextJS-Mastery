bdee33117cb3b2a8805e6fc2f63b7bcaa1b468ce
ChandraShekhar <csyadav0513@gmail.com>
2024-09-10 02:42:50 +0530

    Refactor: Add useMovie hook to fetch movies based on query

--- usePopcorn/src/App-v2.tsx
+++ usePopcorn/src/App-v2.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState, useRef } from "react";
 import StarRating from "./components/StarRating";
+import { useMovie } from "./hooks/useMovie";
 
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
@@ -10,35 +11,15 @@ const KEY = "30c03942";
 
 export default function App() {
   const [query, setQuery] = useState("");
-  const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState("");
   const [selectedId, setSelectedId] = useState(null);
+
+  const { movies, isLoading, error } = useMovie(query);
 
   // const [watched, setWatched] = useState([]);
   const [watched, setWatched] = useState(() => {
     const data = localStorage.getItem("watched");
     return data ? JSON.parse(data) : [];
   });
-
-  /*
-  useEffect(function () {
-    console.log("After initial render");
-  }, []);
-
-  useEffect(function () {
-    console.log("After every render");
-  });
-
-  useEffect(
-    function () {
-      console.log("D");
-    },
-    [query]
-  );
-
-  console.log("During render");
-*/
 
   function handleSelectMovie(id) {
     setSelectedId((selectedId) => (id === selectedId ? null : id));
@@ -58,57 +39,9 @@ export default function App() {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
 
-  useEffect(() => {
-    localStorage.setItem("watched", JSON.stringify(watched));
-  }, [watched]);
-
-  useEffect(
-    function () {
-      const controller = new AbortController();
-
-      async function fetchMovies() {
-        try {
-          setIsLoading(true);
-          setError("");
-
-          const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
-            { signal: controller.signal }
-          );
-
-          if (!res.ok)
-            throw new Error("Something went wrong with fetching movies");
-
-          const data = await res.json();
-          if (data.Response === "False") throw new Error("Movie not found");
-
-          setMovies(data.Search);
-          setError("");
-        } catch (err) {
-          if (err.name !== "AbortError") {
-            console.log(err.message);
-            setError(err.message);
-          }
-        } finally {
-          setIsLoading(false);
-        }
-      }
-
-      if (query.length < 3) {
-        setMovies([]);
-        setError("");
-        return;
-      }
-
-      handleCloseMovie();
-      fetchMovies();
-
-      return function () {
-        controller.abort();
-      };
-    },
-    [query]
-  );
+   useEffect(() => {
+     localStorage.setItem("watched", JSON.stringify(watched));
+   }, [watched]);
 
   return (
     <>
