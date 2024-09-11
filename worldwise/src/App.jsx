55c1d77ad490d4dd83b122a7ac9ef2602aa6cbc5
ChandraShekhar <csyadav0513@gmail.com>
2024-09-11 20:12:49 +0530

    add router and pages

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,7 +1,19 @@
-import "./App.css";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Products from "./pages/Products";
+import HomePage from "./pages/HomePage";
+import Pricing from "./pages/Pricing";
+import NotFound from "./pages/NotFound";
 function App() {
-  return <>Hello</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
