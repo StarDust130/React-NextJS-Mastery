2bac218fb7755441882382772d2b3f73d24dd2dd
ChandraShekhar <csyadav0513@gmail.com>
2024-09-11 10:26:28 +0530

    Add reducer

--- react-quiz/src/App.jsx
+++ react-quiz/src/App.jsx
@@ -1,11 +1,44 @@
+import { useEffect, useReducer } from "react";
 import Header from "./components/Header";
 import MainComp from "./components/MainComp";
 
+const initialState = {
+  questions: [],
+
+  // loading,  error , ready , active , finished
+  status: " loading",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "dataRecieved":
+      return { ...state, questions: action.data, status: "ready" };
+
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+};
+
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    const fetchQuestions = async () => {
+      try {
+        const response = await fetch("http://localhost:3001/questions");
+        const data = await response.json();
+        dispatch({ type: "dataRecieved", data });
+      } catch (error) {
+        console.error("Error fetching questions: ", error);
+      }
+    };
+    fetchQuestions();
+  }, []);
+
   return (
     <div className="app">
       <Header />
-      <MainComp >
+      <MainComp>
         <h1>React Quiz</h1>
         <p>
           This is a quiz to test your knowledge of React. It is a multiple
@@ -18,4 +51,3 @@ const App = () => {
   );
 };
 export default App;
- 
