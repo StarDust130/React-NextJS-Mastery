f79f207376f69eb68f23d35f0de2d85530cbda6e
ChandraShekhar <csyadav0513@gmail.com>
2024-09-14 17:08:34 +0530

    feat: Update BalanceDisplay to fetch balance from Redux store and handle invalid values

--- redux/src/features/accounts/BalanceDisplay.jsx
+++ redux/src/features/accounts/BalanceDisplay.jsx
@@ -1,12 +1,20 @@
+import { useSelector } from "react-redux";
+
 function formatCurrency(value) {
+  // Ensure that value is a number and provide a default of 0 if it's undefined or NaN
+  const validValue = typeof value === "number" && !isNaN(value) ? value : 0;
+
   return new Intl.NumberFormat("en", {
     style: "currency",
     currency: "USD",
-  }).format(value);
+  }).format(validValue);
 }
 
 function BalanceDisplay() {
-  return <div className="balance">{formatCurrency(123456)}</div>;
+  // Use useSelector to get balance from Redux store
+  const balance = useSelector((state) => state.account.balance);
+
+  return <div className="balance">{formatCurrency(balance)}</div>;
 }
 
 export default BalanceDisplay;
