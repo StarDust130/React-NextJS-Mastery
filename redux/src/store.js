a4e44942005db15f40b7f4ce34bf2d44acfa1f47
ChandraShekhar <csyadav0513@gmail.com>
2024-09-14 23:57:06 +0530

    feat: Upgrade to @reduxjs/toolkit and refactor store configuration

--- redux/src/store.js
+++ redux/src/store.js
@@ -1,13 +1,33 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
+// import { createStore, combineReducers, applyMiddleware } from "redux";
+// import accountReducer from "./features/accounts/accountSlice";
+// import customerReducer from "./features/customers/customerSlice";
+// import { thunk } from "redux-thunk";
+// import { composeWithDevTools } from "redux-devtools-extension";
+
+// const rootReducer = combineReducers({
+//   account: accountReducer,
+//   customer: customerReducer,
+// });
+
+// const store = createStore(
+//   rootReducer,
+//   composeWithDevTools(applyMiddleware(thunk))
+// );
+
+// export default store;
+
+//! New Way to create store
+
+import { configureStore } from "@reduxjs/toolkit";
+
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
-import {thunk} from "redux-thunk";
 
-const rootReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReducer,
+  },
 });
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
