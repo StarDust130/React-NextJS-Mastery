54effc883cc5180db4c6de148711402992735bae
ChandraShekhar <csyadav0513@gmail.com>
2024-09-14 15:14:05 +0530

    feat: Update Redux actions for account management and loan processing

--- redux/src/store.js
+++ redux/src/store.js
@@ -6,21 +6,29 @@ const initalState = {
 
 const reducer = (state = initalState, action) => {
   switch (action.type) {
-    case "DEPOSIT":
+    case "account/deposit":
       return {
         ...state,
         balance: state.balance + action.payload,
       };
-    case "WITHDRAW":
+    case "account/withdraw":
       return {
         ...state,
         balance: state.balance - action.payload,
       };
-    case "APPLY_LOAN":
+    case "account/requestLoan":
+      if (state.loan > 0) return state;
       return {
         ...state,
         loan: state.loan + action.payload,
-        loanPurpose: action.loanPurpose,
+        loanPurpose: action.purpose,
+      };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
       };
     default:
       return state;
