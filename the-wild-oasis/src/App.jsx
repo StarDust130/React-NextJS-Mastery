6ebcf0bdbf6bec0991f07ef4ce48b0c8f1334599
ChandraShekhar <csyadav0513@gmail.com>
2024-09-16 13:37:32 +0530

    Add styled-components dependency and update App component

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -1,7 +1,16 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 1.5em;
+  font-weight: bold;
+`;
+
 function App() {
   return (
     <>
-      <div>Hello</div>
+      <div>
+        <H1>Welcome to The Wild Oasis</H1>
+      </div>
     </>
   );
 }
