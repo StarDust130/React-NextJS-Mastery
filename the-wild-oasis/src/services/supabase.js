9822a66584662157d625e1788065f0685bc6b9e0
ChandraShekhar <csyadav0513@gmail.com>
2024-09-17 20:46:46 +0530

    Refactor styled FileInput component to use type attribute

--- the-wild-oasis/src/services/supabase.js
+++ the-wild-oasis/src/services/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://bkdbtknyrgbrahmleiak.supabase.co";
+export const supabaseUrl = "https://dclaevazetcjjkrzczpc.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJrZGJ0a255cmdicmFobWxlaWFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY0Nzk1NTEsImV4cCI6MjA0MjA1NTU1MX0.hiEW0vBbwC9_cYG2T6aJIc4Z44yEEnAf-g6CeRjnwmU";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjbGFldmF6ZXRjamprcnpjenBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMyOTIzNDQsImV4cCI6MTk5ODg2ODM0NH0.LGg0M-taoHgKtxCzr9owrb09epnPaO_Yfz6xVE54sIY";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
--- the-wild-oasis/src/ui/Table.jsx
+++ the-wild-oasis/src/ui/Table.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Table = styled.div`
+const StyledTable = styled.div`
   border: 1px solid var(--color-grey-200);
 
   font-size: 1.4rem;
@@ -9,7 +9,7 @@ export const Table = styled.div`
   overflow: hidden;
 `;
 
-export const CommonRow = styled.header`
+const CommonRow = styled.header`
   display: grid;
   grid-template-columns: ${(props) => props.columns};
   column-gap: 2.4rem;
@@ -17,7 +17,7 @@ export const CommonRow = styled.header`
   transition: none;
 `;
 
-export const StyledHeader = styled(CommonRow)`
+const StyledHeader = styled(CommonRow)`
   padding: 1.6rem 2.4rem;
 
   background-color: var(--color-grey-50);
@@ -28,11 +28,11 @@ export const StyledHeader = styled(CommonRow)`
   color: var(--color-grey-600);
 `;
 
-export const StyledBody = styled.section`
+const StyledBody = styled.section`
   margin: 0.4rem 0;
 `;
 
-export const StyledRow = styled(CommonRow)`
+const StyledRow = styled(CommonRow)`
   padding: 1.2rem 2.4rem;
 
   &:not(:last-child) {
@@ -40,7 +40,7 @@ export const StyledRow = styled(CommonRow)`
   }
 `;
 
-export const Footer = styled.footer`
+const Footer = styled.footer`
   background-color: var(--color-grey-50);
   display: flex;
   justify-content: center;
@@ -51,7 +51,7 @@ export const Footer = styled.footer`
   }
 `;
 
-export const Empty = styled.p`
+const Empty = styled.p`
   font-size: 1.6rem;
   font-weight: 500;
   text-align: center;
