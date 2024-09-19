66810a80650625e3d19b33e9220de251f1d8fed3
ChandraShekhar <csyadav0513@gmail.com>
2024-09-19 10:46:19 +0530

    Refactor font imports and update layout styles

--- nextjs/app/layout.tsx
+++ nextjs/app/layout.tsx
@@ -1,16 +1,10 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Josefin_Sans } from "next/font/google";
 import "@/app/_styles/globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const josefinSans = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -25,11 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${josefinSans} antialiased`}>{children}</body>
     </html>
   );
 }
