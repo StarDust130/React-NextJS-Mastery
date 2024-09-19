db9f22aa961c062beadc5b4009e1f47aaca0687e
ChandraShekhar <csyadav0513@gmail.com>
2024-09-19 14:06:17 +0530

    Refactor file paths and imports for ReservationCard component ``

--- nextjs/app/_components/SideNavigation.tsx
+++ nextjs/app/_components/SideNavigation.tsx
@@ -4,6 +4,7 @@ import {
   UserIcon,
 } from '@heroicons/react/24/solid';
 import SignOutButton from './SignOutButton';
+import Link from 'next/link';
 
 const navLinks = [
   {
@@ -25,21 +26,21 @@ const navLinks = [
 
 function SideNavigation() {
   return (
-    <nav className='border-r border-primary-900'>
-      <ul className='flex flex-col gap-2 h-full text-lg'>
+    <nav className="border-r border-primary-900">
+      <ul className="flex flex-col gap-2 h-full text-lg">
         {navLinks.map((link) => (
           <li key={link.name}>
-            <a
+            <Link
               className={`py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200`}
               href={link.href}
             >
               {link.icon}
               <span>{link.name}</span>
-            </a>
+            </Link>
           </li>
         ))}
 
-        <li className='mt-auto'>
+        <li className="mt-auto">
           <SignOutButton />
         </li>
       </ul>
