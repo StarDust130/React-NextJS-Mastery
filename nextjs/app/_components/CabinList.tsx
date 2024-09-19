178e04cc77c04d631445cbf8655cdd697cb369f3
ChandraShekhar <csyadav0513@gmail.com>
2024-09-19 20:54:23 +0530

    Add CabinList component to display a list of cabins

---
+++ nextjs/app/_components/CabinList.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { getCabins } from "../_lib/data-service";
+import CabinCard from "./CabinCard";
+
+const CabinList = async () => {
+  const cabins = await getCabins();
+
+  if (!cabins) return null;
+  return (
+    <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
+      {cabins.map((cabin: any) => (
+        <CabinCard cabin={cabin} key={cabin.id} />
+      ))}
+    </div>
+  );
+};
+export default CabinList;
