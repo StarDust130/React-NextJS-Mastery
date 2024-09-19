9607cf2bb83c0163e8e9d438ce9fee805a29e833
ChandraShekhar <csyadav0513@gmail.com>
2024-09-19 11:09:41 +0530

    Refactor header component and update logo and navigation files

--- nextjs/app/layout.tsx
+++ nextjs/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Josefin_Sans } from "next/font/google";
 import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
 
 const josefinSans = Josefin_Sans({
   subsets: ["latin"],
@@ -19,7 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${josefinSans.className} antialiased`}>{children}</body>
+      <body className={`${josefinSans.className} antialiased`}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
