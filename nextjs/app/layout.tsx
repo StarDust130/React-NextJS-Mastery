c3df63123b433008cd75bb3297860614719ebbbb
ChandraShekhar <csyadav0513@gmail.com>
2024-09-19 10:28:16 +0530

    Refactor file structure and update CSS styles

--- nextjs/app/layout.tsx
+++ nextjs/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-// import "@/app/_styles/global.css";
+import "@/app/_styles/globals.css";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
